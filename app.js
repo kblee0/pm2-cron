505e97436ef59bb8bb9be7ce0af577ee9d12e602
Kyeongbok Lee <kblee0@gmail.com>
2025-07-01 01:13:24 +0900

    update

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-var pmx     = require('pmx');
+const util = require('util');
+util._extend = Object.assign;
+
+var pmx     	= require('pmx');
+var pm2     	= require('pm2');
+var scheduler	= require('node-schedule');
 
 require('console-stamp')(console, { format: ':date(yyyy/mm/dd HH:MM:ss.l)' });
 
-console.info('[pm2-cron] Starting....', 'PID:', process.pid);
+var conf = pmx.initModule({});
 
-var conf    = pmx.initModule({
-    // Override PID to be monitored
-    pid  : pmx.resolvePidPaths(['/var/run/redis.pid']),
-}, (err, conf) => {
-    console.info("[pm2-cron] configuration =", conf);
-    require('./cron.js')(conf);
+pm2.connect(function(err) {
+    if (err) {
+        console.error(err)
+        process.exit(1);
+    }
+    console.info("PM2 Connected...");
+    schedule_jobs(conf.module_conf);
 });
 
+process.on('exit',() => {
+    console.info("pm2 disconnect");
+    pm2.disconnect()
+});
+
+function schedule_jobs(cron) {
+    Object.keys(cron).forEach((job) => {
+        console.log('schedule job: ', job, "cron", cron[job]);
+        scheduler.scheduleJob(cron[job], () => {
+            pm2.restart(job, (err, proc) => {
+                if (err) {
+                    console.error(`${job} job start failed.`, err);
+                    return;
+                }
+                console.info(`${job} job started.`);
+            });
+        });
+    });
+}
